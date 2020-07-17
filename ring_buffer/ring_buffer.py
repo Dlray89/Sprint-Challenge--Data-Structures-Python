a15ffd6b406abc5f89f981295cba4db7aa8db660
DAP <dlrayjr89@gmail.com>
2020-07-17 16:29:19 +0000

    ring buffer doen

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        self.index = 0
 
     def append(self, item):
-        pass
+        if len(self.data) == self.capacity:
+            print(f"data: {self.data}, index: {self.index}")
+            self.data[self.index] = item
+
+            if [self.index] == [self.capacity - 1]:
+                self.index = 0
+            else:
+                self.index += 1
+        else:
+            self.data.append(item)
 
     def get(self):
-        pass
+        return self.data
